5d7962190e3a8e174f572033bf8f794d481ec3ff
Abhishek Phulari <webeyes02@gmail.com>
2025-02-14 20:06:06 +0530

    Updated vercel.json file

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require("cors");
+const port = 8080;
 
 app.use(cors({
   origin: "*", // Allow all origins (not recommended for production)
@@ -22,4 +23,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
