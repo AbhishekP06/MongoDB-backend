18ecde33531899719c2bbc4e81638aa0dae94964
Abhishek Phulari <webeyes02@gmail.com>
2025-02-15 09:38:48 +0530

    Updated timeouts

--- api/db.js
+++ api/db.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
       const conn = await mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
+        serverSelectionTimeoutMS: 5000,  // Timeout after 5 sec if no response
+        socketTimeoutMS: 45000, // Close socket after 45 sec if no response
       });
       console.log(`MongoDB Connected: {conn.connection.host}`);
     } catch (error) {
